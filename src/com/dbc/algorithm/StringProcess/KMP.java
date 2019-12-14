74bc92679438f0e74170c7c3ace61230dbead4f2
DBC <1210212670@qq.com>
2019-12-15 00:17:46 +0800

    KMP

--- src/com/dbc/algorithm/StringProcess/KMP.java
+++ src/com/dbc/algorithm/StringProcess/KMP.java
@@ -27,4 +27,22 @@ public class KMP {
             else kmpNext[i] = j;
         }
     }
+
+    private int next[] = new int[10010];
+    int KMP_Count(char x[], int m, char y[], int n){
+        //x是模式串，y是主串
+        int i, j;
+        int ans = 0;
+        kmp_pre(x,m,next);
+        i = j = 0;
+        while (i < n){
+            while (-1 != j && y[i] != x[j]) j = next[j];
+            i++;j++;
+            if(j >= m){
+                ans++;
+                j = next[j];
+            }
+        }
+        return ans;
+    }
 }
